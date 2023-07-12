9ffa3715056d044c646937b0daf03c8a12bb9f0f
Oleg <66309663+Insid1@users.noreply.github.com>
2023-07-12 20:35:50 +0700

    feat: adds react

--- config/build/build-loaders.ts
+++ config/build/build-loaders.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 
 const buildLoaders = (): webpack.RuleSetRule[] => {
 
+  // if we don't use typescript then we need to add babel to transpile jsx
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
--- src/test.ts
+++
@@ -1,3 +0,0 @@
-import {testFn} from "./index";
-
-testFn(1)
